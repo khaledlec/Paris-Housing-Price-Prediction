8bfad591bfcf9e236c09cc9d740b8e2840eb1e22
khaled lecheheb <khaled.lecheheb@aivancity.education>
2023-03-18 02:14:05 +0100

    predicting with web applicatin

--- app.py
+++ app.py
@@ -14,22 +14,14 @@ def home():
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data=request.json['data']
+    data=pd.json_normalize(request.json['data'])
+    
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
+    #print(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(data)
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=np.array(data).reshape(1,-1)
-    print(final_input)
-    output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
-
-
-
 if __name__=="__main__":
     app.run(debug=True)
+
