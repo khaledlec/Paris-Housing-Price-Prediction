7341269452c96a350c317fa0d898945c3049d469
khaled lecheheb <khaled.lecheheb@aivancity.education>
2023-04-23 16:06:21 +0200

    Before last version

--- app.py
+++ app.py
@@ -2,25 +2,38 @@ import json
 import pickle
 import pandas as pd
 
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask, request, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
-## Load the model
-model=pickle.load(open('model.pkl','rb'))
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open('model.pkl', 'rb'))
+
+# Load the encoder
+with open('encoder.pkl', 'rb') as f:
+    encoder = pickle.load(f)
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data=pd.json_normalize(request.form)
-    print(data)
-    output=model.predict(data)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    # Read form data into a Pandas dataframe
+    data = pd.json_normalize(request.form)
 
+    # Convert 'Voie' column to lowercase
+    data['Voie'] = data['Voie'].str.lower()
 
-if __name__=="__main__":
+    # Encode 'Voie' column
+    data['Voie'] = encoder.transform(data[['Voie']])
+
+    # Make the prediction
+    prediction = model.predict(data)[0]
+
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(prediction))
+
+if __name__ == "__main__":
     app.run(debug=True)
-
